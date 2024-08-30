c93e3300b44fd6686bad16469975cd8cda7199f5
mila cabrera <valentingalarza2004@gmail.com>
2024-08-29 21:09:53 -0300

    creacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Encabezado from './components/Encabezado.jsx'
+import Formulario from './components/Formulario.jsx'
+import List_tarea from './components/List_tarea.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Encabezado/>
+      <Formulario/>
+      <List_tarea/>
+    </>
+  )
+}
+
+export default App
